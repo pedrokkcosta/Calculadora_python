1ad26a01b55430349d7d099b285406de9283d206
Pedro Costa <phsc2310@gmail.com>
2024-04-30 14:32:59 -0300

    Iniciando Calculadora

---
+++ index.py
@@ -0,0 +1,24 @@
+def verificarFinalizacao():
+    verificacao = input("Você deseja calcular novamente?(s/n)")
+    if verificacao == "s":
+        calcular()
+    elif verificacao == "n":
+        print("Fechando Calculadora")
+    else:
+        verificarFinalizacao()
+def calcular():
+    numero1 = int (input("Digite um numero: "))
+    numero2 = int (input("Digite outro numero: "))
+
+    tipoOperacao = input("Qual o tipo de operação?(+, -, *, /)")
+    if tipoOperacao == "+":
+        print("O resultado de {} + {} é igual a {}".format(numero1, numero2, numero1 + numero2))
+    elif tipoOperacao == "-":
+        print("O resultado de {} - {} é igual a {}".format(numero1, numero2, numero1 - numero2))
+    elif tipoOperacao == "*":
+        print("O resultado de {} * {} é igual a {}".format(numero1, numero2, numero1 * numero2))
+    elif tipoOperacao == "/":
+        print("O resultado de {} / {} é igual a {}".format(numero1, numero2, numero1 / numero2))
+    verificarFinalizacao()
+
+calcular()
